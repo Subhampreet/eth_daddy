72fd4fca8694028b8e0aba3d3ba1fa52807f28e3
Subhampreet <subhampreet18@gmail.com>
2023-01-15 23:16:44 +0530

    🚬 Smoke Test

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,17 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
-
+  it('has a name', async() => {
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy');
+    // deploying to blockchain
+    let ethDaddy = await ETHDaddy.deploy();
+    // calling the name() function
+    const result = await ethDaddy.name();
+    expect(result).to.equal('ETH Daddy');
+  })
 })
