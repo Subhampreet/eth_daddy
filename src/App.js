8809b27aa836bdd26cc902f677cc02b3882019f6
Subhampreet <subhampreet18@gmail.com>
2023-01-29 02:00:36 +0530

    📅 Building Website 0.1

--- src/App.js
+++ src/App.js
@@ -13,6 +13,17 @@ import ETHDaddy from './abis/ETHDaddy.json'
 import config from './config.json';
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async() => {
+    const accounts = await window.ethereum.request({ 'method' : 'eth_requestAccounts'})
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
@@ -20,6 +31,7 @@ function App() {
       <div className='cards__section'>
 
         <h2 className='cards__title'>Welcome to ETH Daddy</h2>
+        <p>{account}</p>
 
       </div>
 
