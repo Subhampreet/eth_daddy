e2341e7d8276294af7574cc16741b108d8200ae1
Subhampreet <subhampreet18@gmail.com>
2023-01-30 23:48:38 +0530

    🎉 Navbar Completed

--- src/App.js
+++ src/App.js
@@ -28,10 +28,11 @@ function App() {
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+
       <div className='cards__section'>
 
         <h2 className='cards__title'>Welcome to ETH Daddy</h2>
-        <p>{account}</p>
 
       </div>
 
